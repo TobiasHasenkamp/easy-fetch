538366245c37ebcb2c4f38d922238bd885fa27b8
TobiasHasenkamp <tobias.hasenkamp@googlemail.com>
2020-10-15 15:53:00 +0200

    created cool new functions.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import useCharacterListHook from "./hooks/useCharacterListHook";
 
 
 function App() {
 
-    const characterList = useCharacterListHook();
+    const [endpoint, setEndpoint] = useState("character");
+    const [count, setCount] = useState(1);
+    const itemList = useCharacterListHook(count, endpoint);
+
+    function selectCorrectItemsToDisplay(endpoint){
+        if (endpoint == "character") {
+            return itemList.map(character => <p>{character.name}, {character.status}</p>)
+        }
+        if (endpoint == "location") {
+            return itemList.map(location => <p>{location.name}, {location.type}</p>)
+        }
+        if (endpoint == "episode") {
+            return itemList.map(episode => <p>{episode.name}, {episode.episode}</p>)
+        }
+
+
+    }
 
   return (
     <div className="App">
 
-        {
-            characterList.map(character => <p>{character.name}, {character.status}</p>)
+        <button onClick = {() => setEndpoint("character")}>Character</button>
+        <button onClick = {() => setEndpoint("location")}>Location</button>
+        <button onClick = {() => setEndpoint("episode")}>Episode</button>
 
+        {selectCorrectItemsToDisplay(endpoint)}
+
+        {/*{itemList.map(character => <p>{character.name}, {character.status}</p>)}*/}
+
+        {count > 1
+            ? <button onClick = {() => setCount(count-1)}> Vorherige Seite ({count-1})</button>
+            : <button disabled> Vorherige Seite </button>
         }
+
+        {/*{count < 20
+            ? */}<button onClick = {() => setCount(count+1)}> Nächste Seite ({count+1}) </button>
+        {/*: <button disabled> Nächste Seite </button>*/
+        }
+        <p>Aktuelle Seite: {count}</p>
 
     </div>
   );
--- src/hooks/useCharacterListHook.js
+++ src/hooks/useCharacterListHook.js
@@ -1,17 +1,17 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
 
-export default function useCharacterListHook(){
+export default function useCharacterListHook(count, endpoint){
 
     const [characterList, setCharacterList] = useState([]);
 
-    const url = "https://rickandmortyapi.com/api/character";
+    const url = "https://rickandmortyapi.com/api/" + endpoint + "/?page=" + count;
     useEffect(() => {
             axios
                 .get(url)
                 .then(response => response.data.results)
                 .then(listOfCharacters => setCharacterList(listOfCharacters))
-        }, []
+        }, [count, endpoint]
     );
 
     return characterList;
